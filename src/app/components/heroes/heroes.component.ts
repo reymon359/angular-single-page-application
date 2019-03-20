1fcbe55063bc9747a684bf4e0e04367bcf609444
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-03-20 16:50:34 +0100

    loaded heroes with ngfor

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService, Hero } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroes',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  heroes: Hero[] = [];
+  constructor(private heroesSer: HeroesService) { }
 
   ngOnInit() {
+    this.heroes = this.heroesSer.getHeroes();
+    console.log(this.heroes);
   }
 
 }
